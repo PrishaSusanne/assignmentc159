37d9043b7f982dcdd39cd6b2de60992406d21655
PrishaSusanne <62793055+PrishaSusanne@users.noreply.github.com>
2022-01-26 12:05:54 +0530

    Add files via upload

---
+++ infoBanner.js
@@ -0,0 +1,111 @@
+AFRAME.registerComponent("info-banner", {
+    schema: {
+      itemId: { default: "", type: "string" },
+    },
+    update: function () {
+      this.createBanner();
+    },
+    createBanner: function () {
+      postersInfo = {
+        captain_america: {
+          banner_url: "./assets/banners/captain_america_banner.jpg",
+          title: "Captain America",
+          released_year: "1941",
+          description:
+            "Captain America is a superhero appearing in American comic books published by Marvel Comics. Created by cartoonists Joe Simon and Jack Kirby, the character first appeared in Captain America Comics #1 (cover dated March 1941) from Timely Comics, a predecessor of Marvel Comics. Captain America was designed as a patriotic supersoldier who often fought the Axis powers of World War II and was Timely Comics' most popular character during the wartime period.",
+        },
+        spiderman: {
+          banner_url: "./assets/banners/spiderman_banner.png",
+          title: "Spiderman",
+          released_year: "1962",
+          description:
+            "Spider-Man is a fictional superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. He first appeared in the anthology comic book Amazing Fantasy (Aug. 1962) in the Silver Age of Comic Books.",
+        },
+        star_wars: {
+          banner_url: "./assets/banners/star_wars_banner.jpg",
+          title: "Star Wars",
+          released_year: "1977",
+          description:
+            "A long time ago in a galaxy far, far away, the adventures of Luke Skywalker, Han Solo, Princess, Leia, Darth Vader and more continue! Set following the events of Episode IV: A New Hope, Marvel's ongoing Star Wars comics reveal new stories featuring these classic characters!",
+        },
+        dr_strange: {
+          banner_url: "./assets/banners/dr_strange_banner.jpg",
+          title: "Dr Strange",
+          released_year: "1963",
+          description:
+            "Doctor Strange, American comic-book superhero created for Marvel Comics by writer Stan Lee and artist Steve Ditko. The character first appeared in a backup strip in Strange Tales no. 110 in July 1963 but soon blossomed into one of the cult characters of the decade and a staple in the Marvel pantheon.",
+        },
+      };
+      const { itemId } = this.data;
+  
+      const fadeBackgroundEl = document.querySelector("#fadeBackground");
+  
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("id", `${itemId}-banner`);
+  
+      entityEl.setAttribute("geometry", {
+        primitive: "plane",
+        width: 0.9,
+        height: 1,
+      });
+  
+      entityEl.setAttribute("material", { color: "#000" });
+      entityEl.setAttribute("position", { x: 0, y: 0.1, z: -1 });
+  
+      const item = postersInfo[itemId];
+  
+      const imageEl = this.createImageEl(item);
+      const titleEl = this.createTitleEl(item);
+      const descriptionEl = this.createDescriptionEl(item);
+  
+      entityEl.appendChild(imageEl);
+      entityEl.appendChild(titleEl);
+      entityEl.appendChild(descriptionEl);
+  
+      fadeBackgroundEl.appendChild(entityEl);
+    },
+    createImageEl: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("geometry", {
+        primitive: "plane",
+        width: 0.85,
+        height: 0.4,
+      });
+      entityEl.setAttribute("material", { src: item.banner_url });
+      entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+      return entityEl;
+    },
+    createTitleEl: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("text", {
+        shader: "msdf",
+        anchor: "left",
+        font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+        width: 1.2,
+        height: 2,
+        color: "#fff",
+        value: `${item.title} (${item.released_year})`,
+      });
+      entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+      return entityEl;
+    },
+    createDescriptionEl: function (item) {
+      const entityEl = document.createElement("a-entity");
+      entityEl.setAttribute("visible", true);
+      entityEl.setAttribute("text", {
+        shader: "msdf",
+        anchor: "left",
+        font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+        width: 0.75,
+        height: 2,
+        color: "#fff",
+        wrapCount: "40",
+        value: item.description,
+      });
+      entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+      return entityEl;
+    },
+  });
